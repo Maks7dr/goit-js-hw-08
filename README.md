f9d2798b3f5957bd7a4280206b9e9359445d00a4
Maksym Drachenko <maks7d@gmail.com>
2024-12-27 16:47:55 +0500

    add task-8

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# goit-js-hw-07
+# goit-js-hw-08
 
-hwjs7
+hwjs8
--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector('#name-input');
-const textName = document.querySelector('#name-output');
-textInput.addEventListener('input', event => {
-  const text = event.target.value.trim();
-  textName.textContent = text ? text : 'Anonymous';
-});
